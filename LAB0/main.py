18544a95d24dd9ea5e4ffb45db31627e6f20235a
WissemAmr <kw_amar@esi.dz>
2024-10-02 13:00:24 +0100

    added first script

---
+++ LAB0/main.py
@@ -0,0 +1,16 @@
+import nltk
+import spacy
+import gensim
+# import stanfordnlp
+import textblob
+import wordcloud
+import transformers
+
+
+print(f"nltk version : {nltk.__version__}")
+print(f"spacy version : {spacy.__version__}")
+print(f"gensim version : {gensim.__version__}")
+# print(f"stanfordnlp version : {stanfordnlp.__version__}")
+# print(f"textblob version : {textblob.__version__}")
+print(f"wordcloud version : {wordcloud.__version__}")
+print(f"transformers version : {transformers.__version__}")
